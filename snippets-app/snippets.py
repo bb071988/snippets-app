61d9c0031f8194511b8e51cb157cef7298a3471c
Bob Braico <bb071988@gmail.com>
2015-03-11 05:00:49 +0000

    updated parsers

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -1,4 +1,7 @@
 import logging
+import sys
+import argparse
+
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
@@ -22,7 +25,50 @@ def get(name):
     Returns the snippet.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+    return name
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    
+#    arguments = parser.parse_args(sys.argv[1:])
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
 
 
+    # Subparser for the get command
+    logging.debug("Constructing Get subparser")
+    get_parser = subparsers.add_parser("get", help="Get a snippet from storage")
+    get_parser.add_argument("name", help="The name of the snippet")
+
+    
+    arguments = parser.parse_args(sys.argv[1:])
+    
+    # Convert parsed arguments from Namespace to dictionary
+    
+    arguments = vars(arguments)
+    #print arguments
+    command = arguments.pop("command")
+    
+    #print command
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        print("")
+        snippet = get(**arguments)
+        print "Retrieved snippet: {!r}".format(snippet)
+    
+    
+
+if __name__ == "__main__":
+    main()
 
