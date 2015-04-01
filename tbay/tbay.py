184b6f2eb8c26f3eb60837fe8bd31ab8407c6592
Bob Braico <bb071988@gmail.com>
2015-04-01 14:19:58 +0000

    final

--- tbay/tbay.py
+++ tbay/tbay.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, ForeignKey,func
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, ForeignKey,func,desc
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
@@ -48,19 +48,19 @@ Base.metadata.create_all(engine) # create the tables if they don't exist
 bob = User()
 bob.user_name = "bob"
 bob.password = "secret"
-bob.user_id = 1
+# bob.user_id = 1
 # session.add(bob)
 # session.commit()
 
 dom = User()
 dom.user_name = "dom"
 dom.password = "secret"
-dom.user_id = 2
+# dom.user_id = 2
 
 joe = User()
 joe.user_name = "joe"
 joe.password = "secret"
-joe.user_id = 3
+# joe.user_id = 3
 
 session.add_all([bob,dom,joe])
 session.commit()
@@ -77,14 +77,14 @@ session.commit()
 
 # dom bids on the baseball
 dom_bid = Bid()
-dom_bid.bid_id = 1
+# dom_bid.bid_id = 1
 dom_bid.bidder_id = dom.user_id
 dom_bid.price = 8.50
 session.add(dom_bid)
 session.commit()
 
 dom_bid2 = Bid()
-dom_bid2.bid_id = 2
+# dom_bid2.bid_id = 2
 dom_bid2.bidder_id = dom.user_id
 dom_bid2.price = 10.50
 session.add(dom_bid2)
@@ -92,14 +92,14 @@ session.commit()
 
 # joe bids on the baseball
 joe_bid = Bid()
-joe_bid.bid_id = 3
+# joe_bid.bid_id = 3
 joe_bid.bidder_id = joe.user_id
 joe_bid.price = 9.25
 session.add(joe_bid)
 session.commit()
 
 joe_bid2 = Bid()
-joe_bid2.bid_id = 4
+# joe_bid2.bid_id = 4
 joe_bid2.bidder_id = joe.user_id
 joe_bid2.price = 12.25
 session.add(joe_bid2)
@@ -135,22 +135,30 @@ for b in bids:
     print b.bid_id,b.price
     
 print "Max bid"
-print max_qry
-# for m in max_qry:
-#     print max_price
-    
+bid_list = session.query(Bid.price).all()
+max_price = max(bid_list)
+print bid_list
+print max_price[0]  #didn't expect this to return a tuple
 
-# Finds the user with the primary key equal to 1
-session.query(User).get(1)
+# get max from db using orderby
+max_order = session.query(Bid.price, Bid.bid_id).order_by(desc(Bid.price)).all()
+print max_order
+print max_order[0][0]
 
-# Returns a list of all of the usernames in ascending order
-session.query(User.user_name).order_by(User.user_name).all()
 
-# Returns the description of all of the basesballs
-session.query(Item.description).filter(Item.item_name == "baseball").all()
+# # Keeping the following items for future reference.
 
-# Return the item id and description for all baseballs which were created in the past.  Remember to import the datetime object: from datetime import datetime
-session.query(Item.item_id, Item.description).filter(Item.item_name == "baseball", Item.start_time < datetime.utcnow()).all()
+# # Finds the user with the primary key equal to 1
+# session.query(User).get(1)
+
+# # Returns a list of all of the usernames in ascending order
+# session.query(User.user_name).order_by(User.user_name).all()
+
+# # Returns the description of all of the basesballs
+# session.query(Item.description).filter(Item.item_name == "baseball").all()
+
+# # Return the item id and description for all baseballs which were created in the past.  Remember to import the datetime object: from datetime import datetime
+# session.query(Item.item_id, Item.description).filter(Item.item_name == "baseball", Item.start_time < datetime.utcnow()).all()
 
 # # deleting a user
 # user = session.query(User).first()
